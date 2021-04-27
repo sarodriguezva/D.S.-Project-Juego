17e5a3b7fd89a66071e3a092ed5da76d81875dbb
SebMatDo <59572442+SebMatDo@users.noreply.github.com>
2021-04-26 22:58:00 -0500

    Anadido lifo

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.mygdx.game.MainLogic;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
             LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-            config.title = "ESTRUCTURA DE DATOS";
+            config.title = "Untitled";
             config.width = 800;
             config.height = 600;
             new LwjglApplication(new MainLogic(), config);
