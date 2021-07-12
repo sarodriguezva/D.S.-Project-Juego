5b740f4dd76075853aa80d1debd743044dc208df
Santiago Rodríguez Vallejo <sarodriguezva@unal.edu.co>
2021-07-12 01:44:37 -0500

    Implementación AVL y HEAPS Completo

--- core/src/com/mygdx/game/DataStructures/TreeTraversal.java
+++ core/src/com/mygdx/game/DataStructures/TreeTraversal.java
@@ -1,5 +1,4 @@
 package com.mygdx.game.DataStructures;
-
 /*
 class NthGradeTreeNode<T>{
     T data;
@@ -10,6 +9,11 @@ class NthGradeTreeNode<T>{
 }
 */
 
+/**
+ * Clase Nodo Binario. Adecuado para soportar Árboles AVL.
+ * 
+ * @author Santiago Rodrí­guez Vallejo
+ */
 class BinaryTreeNode<T>{
     T data;
     BinaryTreeNode<T> leftSon;
@@ -23,14 +27,29 @@ class BinaryTreeNode<T>{
     }
 }
 
+/**
+ * Clase Árbol Binario.
+ * Contiene los métodos de recorrido para un árbol.
+ * 
+ * @author Santiago Rodrí­guez Vallejo.
+ */
 class BinaryTree<T>{
     BinaryTreeNode<T> root;
-    //Atributos opcionales.
 
-    void preorder(){
+    /**
+     * Método que recorre el árbol usando preOrder.
+     */
+    public void preorder(){
         this.auxPreorder(this.root);
     }
 
+    /**
+     * Método que recorre el árbol
+     * imprimiendo primero el contenido del nodo padre,
+     * luego imprime el contenido del hijo izquierdo.
+     * luego imprime el contenido del hijo derecho.
+     * @param auxRoot Nodo raÃ­z a partir del cual se recorre el árbol.
+     */
     private void auxPreorder(BinaryTreeNode<T> auxRoot){
         if (auxRoot != null){
             System.out.print(auxRoot.data + " ");
@@ -39,10 +58,19 @@ class BinaryTree<T>{
         }
     }
 
+    /**
+     * Método que recorre el árbol usando posOrder.
+     */
     public void posorder(){
         this.auxPosorder(this.root);
     }
 
+    /**
+     * Método que recorre el árbol
+     * imprimiendo primero el contenido de los nodos hijos
+     * y luego imprime el contenido del nodo padre.
+     * @param auxRoot Nodo raí­z a partir del cual se recorre el árbol.
+     */
     private void auxPosorder(BinaryTreeNode<T> auxRoot){
         if (auxRoot != null){
             this.auxPosorder(auxRoot.leftSon);
@@ -51,10 +79,20 @@ class BinaryTree<T>{
         }
     }
 
+    /**
+     * Método que recorre el árbol usando inOrder.
+     */
     public void inorder(){
         this.auxInorder(this.root);
     }
 
+    /**
+     * Método que recorre el árbol
+     * imprimiendo primero el contenido del hijo izquierdo,
+     * luego imprime el contenido del nodo padre.
+     * luego imprime el contenido del hijo derecho.
+     * @param auxRoot Nodo raí­z a partir del cual se recorre el árbol.
+     */
     private void auxInorder(BinaryTreeNode<T> auxRoot){
         if (auxRoot != null){
             this.auxInorder(auxRoot.leftSon);
@@ -63,10 +101,18 @@ class BinaryTree<T>{
         }
     }
 
+    /**
+     * Método que recorre el árbol usando levelOrder.
+     */
     public void levelorder(){
         this.auxLevelorder(this.root);
     }
 
+    /**
+     * Método que recorre el árbol
+     * imprimiendo todos los nodos en un mismo nivel a la vez.
+     * @param auxRoot Nodo raíz a partir del cual se recorre el árbol.
+     */
     private void auxLevelorder(BinaryTreeNode<T> auxRoot){
         MyQueue<BinaryTreeNode<T>> Q = new MyQueue<>();
         Q.enqueue(auxRoot);
