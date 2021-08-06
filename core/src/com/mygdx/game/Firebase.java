1cfe0b6d30fe85ba77b9a8595e0c302b89f7133a
SebMatDo <59572442+SebMatDo@users.noreply.github.com>
2021-08-06 12:56:38 -0500

    Error de Pair.

--- core/src/com/mygdx/game/Firebase.java
+++ core/src/com/mygdx/game/Firebase.java
@@ -5,8 +5,11 @@ import com.google.api.core.ApiFutures;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 
 import com.google.firebase.FirebaseApp;
@@ -14,8 +17,11 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mygdx.game.DataStructures.MyDoubleLinkedList;
 import com.mygdx.game.DataStructures.MyHashTable;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import jdk.internal.net.http.common.Pair;
 
 /**
  * Clase para hacer una integracion muy simple con Firebase
@@ -81,5 +87,17 @@ public class Firebase {
              System.out.println("Update fallido");
          }
      }   
-    
+     public MyDoubleLinkedList<Pair<String,String>> searchData(String col) throws InterruptedException, ExecutionException{
+         MyDoubleLinkedList<Pair<String,String>> toReturn = new MyDoubleLinkedList<>();
+         CollectionReference puntajes = bd.collection(col);
+         ApiFuture<QuerySnapshot> qp = puntajes.get();
+         
+         for (DocumentSnapshot dc : qp.get().getDocuments()){
+             String id = dc.getId();
+             String json = (String) dc.get("json");
+             Pair<String,String> tuple = new Pair<>(id,json);
+             toReturn.add(tuple);
+         }     
+         return toReturn;
+     }
 }
